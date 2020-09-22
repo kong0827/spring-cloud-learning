7f7febac2100dc43a03ca8581fb1c66ddecbf870
kong0827 <1351882069@qq.com>
2020-09-22 08:41:29 +0800

    eureka server

---
+++ micro-service-cloud-eureka-server-8001/src/main/java/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author kxj
+ * @date 2020/9/22 0:12
+ * @desc
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
